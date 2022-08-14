d167c5429377648f9f5599462f2c30567d69fa4e
BrendonLH <Brendonhampton1988@gmail.com>
2022-08-13 23:49:20 -0700

    finished testing for coaches

--- wukong_fitness/tests.py
+++ wukong_fitness/tests.py
@@ -26,5 +26,14 @@ class CoachesTests(TestCase):
     def setUp(self):
         self.coach = Coaches.objects.create(First='Ellie', Last='Hampton', Age=11)
 
-    def test_coach_name(self):
+    def test_coach_name_first(self):
         self.assertEqual(f'{self.coach.First}', 'Ellie')
+
+    def test_coach_name_first_fail(self):
+        self.assertIsNot(f'{self.coach.First}', 'May')
+
+    def test_coach_name_last(self):
+        self.assertEqual(f'{self.coach.Last}', 'Hampton')
+
+    def test_coach_age(self):
+        self.assertEqual(self.coach.Age, 11)
