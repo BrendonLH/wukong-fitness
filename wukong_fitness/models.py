10bfc9d3f54647aa2ffc7b29db4e7a79a7f6aad8
BrendonLH <Brendonhampton1988@gmail.com>
2022-08-13 23:15:38 -0700

    added 4 tests to test for object creation and testing for expected failures

--- wukong_fitness/models.py
+++ wukong_fitness/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Equipment(models.Model):
     name = models.CharField(max_length=50)
-    description = models.TextField(" ")
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    tags = models.CharField(max_length=100, default=" ")
+    description = models.TextField(" ", blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    tags = models.CharField(max_length=100, default=" ", blank=True)
 
     def __str__(self):
-        return f"Name: {self.name}, Price: {self.price}"
+        return f"Name: {self.name}"
